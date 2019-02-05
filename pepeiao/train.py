6701303c15c12e08de1a63c76f49f0a0f7981b53
Grady Weyenberg <gradysw@hawaii.edu>
2019-02-04 18:04:43 -1000

    work on train.py, currently bug in the data generator function, must return numpy array of batches!

--- pepeiao/train.py
+++ pepeiao/train.py
@@ -14,9 +14,16 @@ def _make_parser():
     parser.add_argument('-w', '--width', type=float, default=0.5)
     parser.add_argument('-o', '--offset', type=float, default=0.125)
     parser.add_argument('-q', '--quiet', action='store_true')
+    parser.add_argument('-n', '--num-validation', type=float, default=0.25,
+                        help='An integer number of training files to use as a validation set, or if less than one, use as a proportion.')
+    parser.add_argument('-p', '--proportion-ones', type=float,
+                        help = 'Desired proportion of "one" labels in training/validation data')
+    parser.add_argument('-b', '--batch-size', default=64, type=int,
+                        help = 'Training batch size')
     parser.add_argument('model', choices=pepeiao.util.get_models())
     parser.add_argument('description_file', type=argparse.FileType('r'),
                         help = 'File describing training data. A tab-delimited file with two columns: wav_filename, selection_file')
+    parser.add_argument('output', help='Filename for fitted model in (.h5)')
     return parser
 
 
@@ -26,6 +33,10 @@ def data_generator(train_list, width, offset, desired_prop_ones=None):
     count_ones = 0
     keep_prob = 1.0
     keep = True
+
+    if desired_prop_ones and (desired_prop_ones < 0 or desired_prop_ones > 1):
+        raise ValueError('desired proportion of ones is not a valid proportion.')
+
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         train_list = itertools.cycle(train_list)
@@ -44,7 +55,7 @@ def data_generator(train_list, width, offset, desired_prop_ones=None):
                         keep = True
                         count_ones += 1
                     else:
-                        keep = random.random() < keep_prob:
+                        keep = random.random() < keep_prob
                     if keep:
                         count_total += 1
                         yield (window, label)
@@ -57,24 +68,61 @@ def data_generator(train_list, width, offset, desired_prop_ones=None):
                             keep_prob = max(0.0, keep_prob - 0.05)
                             
             current_feature = next_future
-            
-            
 
-if __name__ == '__main__':
+def grouper(iterable, n, fillvalue=None):
+    'Collect data into fixed-length chunks or blocks (from itertools recipes)'
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+def _main():
     parser = _make_parser()
     logging.basicConfig(level=logging.INFO)
                 
     args = parser.parse_args()
-    print(args)
-
+    _LOGGER.debug(args)
     training_list = list(csv.reader(args.description_file, delimiter='\t'))
-    # for item in training_list:
-    #     print(item)
-                          
-    try:
-        for idx, item in enumerate(data_generator(training_list, args.width, args.offset)):
-            if idx % 100 == 0:
-                print('.', end='', flush=True)
-    except KeyboardInterrupt:
-        pass
 
+    if args.num_validation >= 1.0:
+        if args.num_validation > len(training_list):
+            raise ValueEror('--num-validation argument is greater than the number of available files')
+        n_valid = int(args.num_validation)
+    elif args.num_validation >= 0.0:
+        n_valid = int(args.num_validation * len(training_list))
+    else:
+        raise ValueError('--num-validation argument is not positive')
+
+    if n_valid > 0.3 * len(training_list):
+        _LOGGER.warn('Using more than 30% of files as validation data.')
+
+    training_set = grouper(
+        data_generator(training_list[:-n_valid], args.width, args.offset, args.proportion_ones),
+        args.batch_size)
+    
+    validation_set = grouper(
+        data_generator(training_list[-n_valid:], args.width, args.offset, args.proportion_ones),
+        args.batch_size)
+
+    model_description = pepeiao.util.get_models()[args.model]
+
+    input_shape = next(training_set)[0].shape[1:]
+    
+    model = model_description[model](input_shape)
+    
+    history = model.fit_generator(
+        training_set,
+        steps_per_epoch=200,
+        shuffle=False,
+        epochs=100,
+        verbose=1, #0-silent, 1-progessbar, 2-1line
+        validation_data=validation_set,
+        validation_steps=200,
+        callbacks=[EarlyStopping(patience=5)],
+    )
+    
+    model.save(args.output)
+
+if __name__ == '__main__':
+    _main()
+
+    
